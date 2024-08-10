f068901a5f65bca243d16c3bfc6b8f3609007d19
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2024-08-10 14:20:26 +0600

    components added

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  My App
+  <Header />
+  <div class="container">
+    <Balance />
+  </div>
+
 </template>
+
+<script>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+
+  export default {
+
+    components: {
+      Header,
+      Balance
+    },
+
+  }
+</script>
